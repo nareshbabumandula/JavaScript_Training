d1ec1d926817a55da470c8cd1c2967e6407da7a4
[nareshbabumandula] <naresh223@gmail.com>
2024-11-22 09:54:21 +0530

    Working with dropdown with Playwright

--- PlayWright_Automation/tests/DryRun.spec.js
+++ PlayWright_Automation/tests/DryRun.spec.js
@@ -1,12 +1,15 @@
 import {test, expect} from '@playwright/test'
 import { link } from 'fs';
 /**
- * The async kwyowrd is used to define a function that always returns a Promise. Inside an async fucntion, we can use await keyword to pause
+ * The async keyword is used to define a function that always returns a Promise. Inside an async fucntion, we can use await keyword to pause
  * the function execution until a Promise resolves, making asynchronous code look synchronous
  */
 test('Locate and interact with links', async ({page}) => {
-    await page.goto("https://www.mycontactform.com");
+    await page.goto("https://www.mycontactform.com/samples.php");
     await page.waitForTimeout(2000);
     
-    
+    await page.getByLabel('Pre-Defined Field - Countries:').selectOption("India");
+    await page.waitForTimeout(2000);
+    await page.getByLabel('Pre-Defined Field - Countries:').selectText("Greenland");
+    await page.waitForTimeout(2000);
 });
